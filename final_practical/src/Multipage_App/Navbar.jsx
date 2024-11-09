49e1333afdd7061c240ba42e45012d800a0fee8e
nikhil kiran patkal <nikhilpatkal9552@gmail.com>
2024-11-09 22:10:21 +0530

    feat:checking the process

--- final_practical/src/Multipage_App/Navbar.jsx
+++ final_practical/src/Multipage_App/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
     <nav className="navbar">
       <ul className="nav-list">
         <li className="nav-item">
+          
           <Link to="/">Home</Link>
         </li>
         <li className="nav-item">
